cc36b7ed7eb5b3ad68556f47c5d128a8ffb46712
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-08-24 08:44:25 +0700

    feat: add reject to promise

--- src/utils/requester.ts
+++ src/utils/requester.ts
@@ -6,14 +6,12 @@ let token = "";
 export function get(endpoint: string, params?: string) {
   if (!token) throw new Error("Authorization token has not been set!");
   if (params) endpoint += "?" + params;
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     fetch(address + endpoint, {
       headers: { Authorization: token },
     })
       .then((res) => res.json().then(body => resolve(body)))
-      .catch((error) => {
-        throw error;
-      });
+      .catch((error) => reject(error));
   });
 }
 
